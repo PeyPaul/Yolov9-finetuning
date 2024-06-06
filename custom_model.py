81235e8ace67a2e6469222eaf31733ca1aaaa242
Paul Peytevin <ppeytevin@gmail.com>
2024-06-06 21:57:06 +0200

    it works

--- custom_model.py
+++ custom_model.py
@@ -3,3 +3,5 @@ from ultralytics import YOLO
 model = YOLO('train/weights/best.pt') # Load model
 
 results = model.predict(source = 0, imgsz = 640, conf = 0.3, save = False, show = True) # Run inference on webcam
+# Can also use model.predict('data/images') to run inference on images in a directory, or model.predict('data/video.mp4') for video
+# In that case, set save = True to save the results to a directory
