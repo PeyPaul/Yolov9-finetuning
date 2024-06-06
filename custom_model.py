8e4b31085adeb9a5887786ff656a4dcca42ff0ca
Paul Peytevin <ppeytevin@gmail.com>
2024-06-06 21:55:16 +0200

    it works

--- custom_model.py
+++ custom_model.py
@@ -1,4 +1,5 @@
 from ultralytics import YOLO
 
-model = YOLO('yolov9c-seg.pt') # Load model
+model = YOLO('train/weights/best.pt') # Load model
 
+results = model.predict(source = 0, imgsz = 640, conf = 0.3, save = False, show = True) # Run inference on webcam
